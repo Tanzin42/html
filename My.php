2dec6a6ee8b8bf42130c43d3cb2c39913b8dc721
Tanzin42 <123922005+Tanzin42@users.noreply.github.com>
2023-02-09 21:00:38 +0600

    Add files via upload

---
+++ My.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html><head>
+  <title>Page Title</title>
+</head><body><p>1.Donation 2.confirmation  3.Thank you</p><h1>Donnor Information</h1>
+  <form>
+   <fieldset>
+    <legend>REGISTRATION</legend>
+    <label for="name">First name:</label>
+    <input type="text" id="name" name="name"><hr>
+    
+    
+    <label for="Email">Email:</label>
+    <input type="email" id="Email" name="Email"><hr>
+    
+    <label for="User Name">User name:</label>
+    <input type="text" id="User name" name="User name"><hr>
+    <label for="Password">Password:</label> 
+	
+<input type="password" id="pwd" name="pwd"><hr>
+<label>confirm password:</label><br>
+
+<input type="password" name="confirm_password" id="confirm_password"> <br>
+
+	 
+	  
+     <fieldset>
+    <legend>Gender</legend>
+      
+      
+      <label for="Gender">Male</label>
+      <input type="radio" id="male" name="male" value="emale">
+      <label for="Gender">Female</label>
+      <input type="radio" id="Female" name="Female" value="Female">
+	   <label for="Gender">Other</label>
+      <input type="radio" id="Female" name="Female" value="Female">
+	  </fieldset>
+	  <button> Submit</button>
+	  
+      
+	   </fieldset>
+    </form> 
+  </body>
+  </html>
