c639dddfb5df7057a34ae6d9b77101c9adcc9ea3
Tanzin42 <123922005+Tanzin42@users.noreply.github.com>
2023-02-09 21:07:22 +0600

    Add files via upload

---
+++ My first.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html><head>
+  <title>Page Title</title>
+</head><body><p>1.Donation 2.confirmation  3.Thank you </p><h1>Donnor Information</h1>
+  <form>
+      <label for="fname">First Name*</label>
+      <input type="text" id="fname"* name="fname"*><br><br>
+      <label for="lname">Last Name*</label>
+      <input type="text" id="lname" name="lname"><br><br>
+        <label for="name"> Company*</label>
+        <input type="text" id="Company" name="Company"><br><br>
+       <label for="Address 1">Address1*</label>
+       <input type="text" id="Address1" name="Address1"><br><br>
+        <label for="Address 2">Address2</label>
+       <input type="text" id="Address2" name="Address2"><br><br>
+        <label for="City*">City*</label>
+       <input type="text" id="City*" name="City*"><br><br>
+        <label>State*</label>
+      <select>
+       <option>Select a State </option>
+
+    <option value="DK">DHAKA</option>
+    <option value="B.BARIA">BRAMONBARIA</option>
+    <option value="CU">CUMILLA</option>
+    <option value="CHANDPUR">CHANDPUR</option>
+
+    </select><br><br>
+
+      <label for="Zip Code*">Zip Code*</label>
+       <input type="number" id="Zip Code*" name="Zip Code*"><br><br>
+      
+     <label>Country</label>
+
+      <select>
+       <option>Select a Country </option>
+
+    <option value="DK">DHAKA</option>
+    <option value="IND">INDIA</option>
+    <option value="PAK">PAKISTAN</option>
+  
+    </select><br><br>
+
+      <label for="Phone">Phone</label>
+       <input type="number" name="Phone"><br><br>
+
+      <label for="Fax">Fax</label>
+      <input type="number" name="Fax"><br><br>
+
+      <label for="Email">Email*</label>
+      <input type="Email" id="Email"* name="Email"*><br><br>
+
+
+      <label for="donetion">Donetion Amount</label>
+      <input type="radio" name="number">None
+      <input type="radio" name="number">$50
+      <input type="radio" name="number">$75
+      <input type="radio" name="number">$100
+      <input type="radio" name="number">$250
+      <input type="radio" name="number">Others<br><br>
+
+      <label for="Other amount">Other Amount $</label>
+      <input type="number" name="other amount"><br><br>
+     
+      <label for="rd">Recurring Donetion</label>
+      <input type="radio" name="text"><b>I am interested in giving on a regular basis.<b><br><br>
+
+          <label for="mcc">Monthly credit card $</label>
+            <input type="number" name="Monthly credit card $">
+           <label for="For months">For Months</label>
+                  <input type="number" name="For Months"><br><br>
+
+
+         
+ <h1>Honorarium and Memorial Donation Information</h1>
+
+       <label for="donetion">I would like to make this donation</label>
+      <input type="radio" name="To Honor">To Honor
+       <input type="radio" name="In Memory of">In Memory of<br><br>
+
+      <label for="name">Name</label>
+      <input type="text" id="name" name="Name"><br><br>
+      <label for="Adt">Acknowledege Donation to</label>
+      <input type="text" id="Adt" name="Acknowledege Donation to"><br><br>
+
+       <label for="Address">Address</label>
+       <input type="text" id="Address" name="Address"><br><br>
+
+        <label for="City*">City</label>
+       <input type="text" id="City" name="City"><br><br>
+
+
+        <label>State</label>
+      <select>
+       <option>Select a State </option>
+
+    <option value="DK">DHAKA</option>
+    <option value="B.BARIA">BRAMONBARIA</option>
+    <option value="CU">CUMILLA</option>
+    <option value="CHANDPUR">CHANDPUR</option>
+
+    </select><br><br>
+
+      <label for="Zip Code">Zip Code</label>
+       <input type="number" id="Zip Code" name="Zip Code"><br><br>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </form> 
+  </body>
+  </html>
